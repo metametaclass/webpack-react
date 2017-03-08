824c0316aa7d7c56ed6890ba5e678aa6de3f8d4b
Matylitski Yuri <ym@tekinsoft.com>
2017-03-08 20:06:24 +0300

    stateful component

--- src/app.js
+++ src/app.js
@@ -3,8 +3,8 @@ import ReactDOM from 'react-dom';
 
 import styles from './app.css'
 
-function formatName(user) {
-  console.log("test");
+/*
+function formatName(user) {  
   return user.firstName + ' ' + user.lastName;
 }
 
@@ -19,6 +19,15 @@ const element = (
   </h1>
 );
 
+ReactDOM.render(
+  element,
+  document.getElementById('app')
+);
+
+*/
+
+
+/*
 function tick() {
   const element = (
     <div>
@@ -32,10 +41,126 @@ function tick() {
   );
 }
 
+setInterval(tick, 1000);
+*/
+
+
+/*
+function Welcome(props) {
+  return <h1>Hello, {props.name}</h1>;
+}
+
+function App() {
+  return (
+    <div>
+      <Welcome name="Sara" />
+      <Welcome name="Cahal" />
+      <Welcome name="Edite" />
+    </div>
+  );
+}
+
 
 ReactDOM.render(
-  element,
-  document.getElementById('app')
+  <App/>,
+  document.getElementById('root')
 );
 
-setInterval(tick, 1000);
+*/
+
+
+
+/*
+function formatDate(date) {
+  return date.toLocaleDateString();
+}
+
+function Avatar(props) {
+  return (
+    <img className="Avatar"
+         src={props.user.avatarUrl}
+         alt={props.user.name} />
+  );
+}
+
+function UserInfo(props) {
+  return (
+    <div className="UserInfo">
+      <Avatar user={props.user} />
+      <div className="UserInfo-name">
+        {props.user.name}
+      </div>
+    </div>
+  );
+}
+
+function Comment(props) {
+  return (
+    <div className="Comment">
+      <UserInfo user={props.author} />
+      <div className="Comment-text">
+        {props.text}
+      </div>
+      <div className="Comment-date">
+        {formatDate(props.date)}
+      </div>
+    </div>
+  );
+}
+
+const comment = {
+  date: new Date(),
+  text: 'I hope you enjoy learning React!',
+  author: {
+    name: 'Hello Kitty',
+    avatarUrl: 'http://placekitten.com/g/64/64'
+  }
+};
+ReactDOM.render(
+  <Comment
+    date={comment.date}
+    text={comment.text}
+    author={comment.author} />,
+  document.getElementById('root')
+);*/
+
+class Clock extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {date: new Date()};
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Hello, world!</h1>
+        <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
+      </div>
+    );
+  }
+
+  componentDidMount() {
+  	this.timerID = setInterval(
+      () => this.tick(),
+      1000
+    );
+  }
+
+  componentWillUnmount() {
+  	clearInterval(this.timerID);
+  }
+
+  tick() {
+    this.setState({
+      date: new Date()
+    });
+  }
+}
+
+
+ReactDOM.render(
+    <Clock/>,
+    document.getElementById('root')
+);
+
